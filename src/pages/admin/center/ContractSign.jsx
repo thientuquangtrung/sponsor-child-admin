a2e94c9096e6edc220341c5c99d772b26d32d919
Trung Nguyen <trung@intra.co.kr>
2024-11-03 14:05:29 +0700

    Refactor common function for generatePDF

--- src/pages/admin/center/ContractSign.jsx
+++ src/pages/admin/center/ContractSign.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useRef } from 'react';
 import SignatureCanvas from 'react-signature-canvas';
-import html2canvas from 'html2canvas';
-import { jsPDF } from 'jspdf';
 import { useSelector } from 'react-redux';
 import { Toaster, toast } from 'sonner';
 import { ClipboardCheck, ClipboardPenLine, Eraser, XCircle } from 'lucide-react';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import 'react-datepicker/dist/react-datepicker.css';
 import { useGetContractByIdQuery, useUpdateContractMutation } from '@/redux/contract/contractApi';
 import { useGetCampaignByIdQuery } from '@/redux/campaign/campaignApi';
 import ContractCampaignContent from '@/pages/admin/center/ContractCampaignContent';
 import ContractGuaranteeContent from '@/pages/admin/center/ContractGuaranteeContent';
 import { UPLOAD_FOLDER, UPLOAD_NAME, uploadFile } from '@/lib/cloudinary';
+import { generatePDF } from '@/lib/utils';
 
 const ContractSign = ({ contractID }) => {
     const { user } = useSelector((state) => state.auth);
@@ -51,40 +51,6 @@ const ContractSign = ({ contractID }) => {
         setIsSigned(true);
     };
 
-    const generatePDF = async () => {
-        const element = contractRef.current;
-        const canvas = await html2canvas(element, {
-            scale: 2,
-            logging: false,
-            useCORS: true,
-            scrollY: -window.scrollY,
-        });
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF({
-            orientation: 'portrait',
-            unit: 'mm',
-            format: 'a4',
-            compress: true,
-        });
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        let heightLeft = pdfHeight;
-        let position = 0;
-
-        pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfHeight);
-        heightLeft -= pdf.internal.pageSize.getHeight();
-
-        while (heightLeft >= 0) {
-            position = heightLeft - pdfHeight;
-            pdf.addPage();
-            pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfHeight);
-            heightLeft -= pdf.internal.pageSize.getHeight();
-        }
-
-        return pdf;
-    };
-
     const handleUpdateContract = async (status) => {
         if (!contractDetails) return;
 
@@ -97,7 +63,7 @@ const ContractSign = ({ contractID }) => {
                     let pdfUrl = contractDetails.softContractUrl;
 
                     if (status === 6) {
-                        const pdf = await generatePDF();
+                        const pdf = await generatePDF(contractRef.current);
                         const pdfBlob = pdf.output('blob');
                         const pdfData = await uploadFile({
                             file: pdfBlob,
