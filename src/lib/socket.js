bbdceed7642d5ab59d784184cbc546dc200ec22c
Trung Nguyen <trung@intra.co.kr>
2024-11-18 08:16:17 +0700

    Config: update node server url

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 let socket;
 
 const connectSocket = (user_id, token) => {
-    socket = io(import.meta.env.VITE_APP_SERVER_DOMAIN, {
+    socket = io(import.meta.env.VITE_APP_NODE_SERVER_URL, {
         query: {
             token,
             user_id,
