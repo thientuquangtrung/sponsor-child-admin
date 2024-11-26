4fa7ab027c11162c1b844a92896a1f0495b1da8c
Huu Tri <huutri1572002@gmail.com>
2024-11-26 10:46:37 +0700

    Fix Contract_campaign

--- src/pages/admin/center/ContractCampaignContent.jsx
+++ src/pages/admin/center/ContractCampaignContent.jsx
@@ -56,7 +56,7 @@ const ContractCampaignContent = ({ signatureA, campaignDetails, contractDetails 
             };
         }
 
-        if (guaranteeProfile.guaranteeType === "0") {
+        if (guaranteeProfile.guaranteeType === 0) {
             return (
                 <div className="space-y-2">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
