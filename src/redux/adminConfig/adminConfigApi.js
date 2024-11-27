d508f343928a0b882c51e5ec940223bc2798c692
ngantvh <vohongngan17022003@gmail.com>
2024-11-27 18:15:22 +0700

    Call api admin config

--- src/redux/adminConfig/adminConfigApi.js
+++ src/redux/adminConfig/adminConfigApi.js
@@ -4,12 +4,38 @@ export const adminConfigApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getAdminConfig: builder.query({
             query: () => '/adminConfig',
-        })
+        }),
 
+        createAdminConfig: builder.mutation({
+            query: (data) => ({
+                url: '/adminConfig',
+                method: 'POST',
+                body: data,
+            }),
+        }),
+        
+        setDefaultAdminConfig: builder.mutation({
+            query: (data) => ({
+                url: '/adminConfig/set-default',
+                method: 'POST',
+                body: data,
+            }),
+        }),
+        
+        updateAdminConfig: builder.mutation({
+            query: ({ id, ...data }) => ({
+                url: `/adminConfig/${id}`,
+                method: 'PUT',
+                body: data,
+            }),
+        }),
         
     }),
 });
 
 export const {
-    useGetAdminConfigQuery
+    useGetAdminConfigQuery,
+    useUpdateAdminConfigMutation,
+    useSetDefaultAdminConfigMutation,
+    useCreateAdminConfigMutation
 } = adminConfigApi;
